93e9795a6c955d7ef1b0b1430b48887f426fc2d1
mmulyana <mulyan.t20@gmail.com>
2024-04-14 03:26:41 +0700

    update: sidebar comp and config

--- src/component/layout/sidebar.tsx
+++ src/component/layout/sidebar.tsx
@@ -1,35 +1,79 @@
+'use client'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { ReactNode } from 'react'
+
+import UserProfile from '../ui/user-profile'
+import IconHome from '../icon/icon-home'
+import IconPieChart from '../icon/icon-pie-chart'
+import IconSetting from '../icon/icon-setting'
+
+type menu = {
+  name: string
+  url: string
+  icon: ReactNode
+}
+
+const menus: menu[] = [
+  {
+    name: 'Today',
+    url: '/today',
+    icon: <IconHome />,
+  },
+  {
+    name: 'Analytic',
+    url: '/analytic',
+    icon: <IconPieChart />,
+  },
+  {
+    name: 'Setting',
+    url: '/setting',
+    icon: <IconSetting />,
+  },
+]
 
 export default function Sidebar() {
   return (
-    <div className='h-screen w-[360px] fixed top-0 left-0 px-4'>
-      <div className='h-16 w-full pt-3 flex gap-2'>
-        <div className='w-7 h-7 rounded-full bg-[#745EFF] flex justify-center items-center text-white'>
-          <span className='m-0 text-lg'>K</span>
-        </div>
-        <p className='text-lg font-medium text-[#2e2e2e]'>Kaffah</p>
-      </div>
-      <div className='mt-2 flex flex-col gap-5'>
-        <Link
-          href={'/dashboard'}
-          className='block py-3 px-5 relative bg-white rounded-xl shadow-md shadow-gray-200'
-        >
-          <div className='w-[5px] h-[25px] bg-[#745EFF] rounded-xl absolute top-1/2 -translate-y-1/2 left-0' />
-          <p className='text-[#1F1F21]'>Today</p>
-        </Link>
-        <Link
-          href={'/dashboard'}
-          className='block py-3 px-5 relative rounded-xl hover:bg-white/50'
-        >
-          <p className='text-[#9A9A9A]'>Progress</p>
-        </Link>
-        <Link
-          href={'/dashboard'}
-          className='block py-3 px-5 relative rounded-xl hover:bg-white/50'
-        >
-          <p className='text-[#9A9A9A]'>Checklist</p>
-        </Link>
+    <div className='h-screen w-[320px] fixed top-0 left-0 px-8 pt-5 hidden lg:flex flex-col'>
+      <UserProfile />
+      <div className='flex flex-col mt-8 gap-6'>
+        <LinkItems links={menus} />
       </div>
     </div>
   )
+}
+
+type LinkItemProps = {
+  links: menu[]
+}
+
+function LinkItems(props: LinkItemProps) {
+  const pathname = usePathname()
+
+  return props.links.map((link) => {
+    if (pathname == link.url) {
+      return (
+        <Link
+          key={link.url}
+          href={link.url}
+          className='h-10 w-full flex gap-[10px] items-center px-3 relative bg-white shadow-[0_1.5px_0px_0px_rgba(217,217,217,1)] rounded-[6px]'
+        >
+          <div className='h-6 w-[3px] rounded-r absolute top-1/2 -translate-y-1/2 left-0 bg-primary-light' />
+          <span className='text-primary-light/70'>{link.icon}</span>
+          <span className='text-primary-light'>{link.name}</span>
+        </Link>
+      )
+    }
+    return (
+      <Link
+        key={link.url}
+        href={link.url}
+        className='h-10 w-full flex gap-[10px] items-center px-3 relative rounded-[6px] hover:bg-[#A9A9A9]/10'
+      >
+        <span className='text-neutral-light/30'>{link.icon}</span>
+        <span className='text-neutral-dark/90'>{link.name}</span>
+      </Link>
+    )
+  })
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/component/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/component/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        neutral: {
+          light: '#525252',
+          dark: '#3F3F3F',
+        },
+        primary: {
+          light: '#745EFF',
+        },
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
