b62934da4cb47605aa531a0e79a49032492c9378
mmulyana <mulyan.t20@gmail.com>
2024-04-14 03:24:57 +0700

    feat: add new icon

---
+++ src/component/icon/icon-check.tsx
@@ -0,0 +1,21 @@
+import { IconProps } from './type'
+
+export default function IconCheck({ stroke = 'white', ...props }: IconProps) {
+  return (
+    <svg
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M20 6L9 17L4 12'
+        stroke={stroke}
+        stroke-width='2.5'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+      />
+    </svg>
+  )
+}
