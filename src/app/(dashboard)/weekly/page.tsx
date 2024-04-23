1b7ab96cf1a0738448e8f5a789d59c9aa62a8e5b
mmulyana <mulyan.t20@gmail.com>
2024-04-23 22:13:37 +0700

    feat: fetch data from api

--- src/app/(dashboard)/weekly/page.tsx
+++ src/app/(dashboard)/weekly/page.tsx
@@ -1,7 +1,32 @@
-export default async function Page() {
+import Header from '@/component/ui/header'
+import PrayTracker from '@/component/ui/pray-tracker'
+import { auth } from '@clerk/nextjs'
+
+type Props = {
+  params: {
+    week: string
+  }
+}
+export default async function Page({ params }: Props) {
+  const { userId } = auth()
+  const res = await fetch(`http://localhost:3000/api/pray/${userId}`, {
+    next: { tags: ['pray_log'], revalidate: 3000 },
+  })
+  const { data } = await res.json()
+
   return (
-    <section>
-      <p>Today</p>
+    <section className='pb-10'>
+      {/* <Header index={params.week} handleIndex={handleIndex} /> */}
+      <div className='flex flex-col gap-5 w-full h-fit'>
+        {data?.map((item: any, i: number) => (
+          <PrayTracker
+            userId={userId ?? ''}
+            data={item.data}
+            name={item.name}
+            key={i}
+          />
+        ))}
+      </div>
     </section>
   )
 }
